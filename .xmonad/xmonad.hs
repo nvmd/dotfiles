6e1c92664a95e157fa0c9262b49ad194a1d07c7d
Sergey Kazenyuk <kazenyuk@gmail.com>
2012-06-02 10:28:19 +0400

    XMonad config

---
+++ .xmonad/xmonad.hs
@@ -0,0 +1,28 @@
+import XMonad
+import XMonad.Hooks.DynamicLog
+import XMonad.Util.EZConfig(additionalKeys)
+
+-- main = do
+--	xmonad $ defaultConfig
+--		{ terminal = "urxvt"
+--		}
+
+-- The main function.
+main = xmonad =<< statusBar myBar myPP toggleStrutsKey myConfig
+
+-- Command to launch the bar.
+myBar = "xmobar"
+
+-- Custom PP, configure it as you like. It determines what is being written to the bar.
+myPP = xmobarPP { ppCurrent = xmobarColor "#429942" "" . wrap "<" ">"
+                , ppSep = " | "
+                , ppTitle = xmobarColor "green" "" . shorten 85
+                }
+
+-- Key binding to toggle the gap for the bar.
+toggleStrutsKey XConfig {XMonad.modMask = modMask} = (modMask, xK_b)
+
+-- Main configuration, override the defaults to your liking.
+--myConfig = defaultConfig { modMask = mod4Mask }
+myConfig = defaultConfig { terminal = "urxvt" } `additionalKeys` [ ((mod4Mask, xK_l), spawn "xscreensaver-command -lock") ]
+
