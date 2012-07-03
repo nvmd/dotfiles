e92a7b56247ec0057da9d2630a0373fc283b0ee0
Sergey Kazenyuk <kazenyuk@gmail.com>
2012-07-04 00:33:33 +0400

    XMonad and xmobar configuration to use with dmenu and trayer; floating skype and firefox windows

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,6 +1,8 @@
 import XMonad
 import XMonad.Hooks.DynamicLog
 import XMonad.Util.EZConfig(additionalKeys)
+import XMonad.Hooks.ManageDocks
+import XMonad.Actions.CopyWindow -- for kill1
 
 -- main = do
 --	xmonad $ defaultConfig
@@ -9,6 +11,12 @@ import XMonad.Util.EZConfig(additionalKeys)
 
 -- The main function.
 main = xmonad =<< statusBar myBar myPP toggleStrutsKey myConfig
+
+myManageHook = composeAll . concat $
+           [[ className =? "Skype" --> doFloat ]
+-- Float Firefox dialog windows
+           ,[ (className =? "Firefox" <&&> resource =? "Dialog") --> doFloat ]
+           ]
 
 -- Command to launch the bar.
 myBar = "xmobar"
@@ -27,10 +35,14 @@ dmenuRunCmd = "cmd=$(yeganesh --executables -- -b -f -i -p \"$ \") && exec $cmd"
 
 -- Main configuration
 --myConfig = defaultConfig { modMask = mod4Mask }
-myConfig = defaultConfig { terminal = terminalCmd }
-           `additionalKeys`
+myConfig = defaultConfig {
+             terminal = terminalCmd
+--           , layoutHook = avoidStruts $ layoutHook defaultConfig
+           , manageHook = manageDocks <+> myManageHook <+> manageHook defaultConfig
+           } `additionalKeys`
            [ ((mod4Mask, xK_l), spawn "xscreensaver-command -lock")
            , ((mod1Mask, xK_p), spawn dmenuRunCmd) -- overriding default command
---         , ((mod4Mask, xK_r), spawn dmenuRunCmd) ] -- additional, windows-like
+--         , ((mod4Mask, xK_r), spawn dmenuRunCmd) -- additional, windows-like
+--           , ((mod1Mask .|. shiftMask, xK_c), kill1) -- close the focused window
            ]
 
