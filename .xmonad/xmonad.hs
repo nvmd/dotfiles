49dc332a0344b7ffd6a7cdbae77267ffc303617d
Sergey Kazenyuk <kazenyuk@gmail.com>
2012-09-25 17:03:01 +0400

    Floating windows placement and "float all new" mode

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -4,6 +4,9 @@ import XMonad.Util.EZConfig(additionalKeys)
 import XMonad.Hooks.ManageDocks
 import XMonad.Util.WorkspaceCompare	-- for getSortByXineramaRule
 import XMonad.Hooks.SetWMName
+import XMonad.Layout.SimpleFloat
+import XMonad.Hooks.FloatNext
+import XMonad.Hooks.Place
 
 -- main = do
 --	xmonad $ defaultConfig
@@ -14,7 +17,14 @@ import XMonad.Hooks.SetWMName
 main = xmonad =<< statusBar myBar myPP toggleStrutsKey myConfig
 
 myManageHook = composeAll . concat $
+-- To find the property name associated with a program, use
+-- xprop | grep WM_CLASS
+-- and click on the client you're interested in.
+-- xprop | grep WM_CLASS: WM_CLASS(STRING) = <resource>, <class>
            [[ className =? "Skype" --> doFloat ]
+           ,[ className =? "Xmessage" --> doFloat ]
+           ,[ className =? "Nm-connection-editor" --> doFloat ]
+           ,[(className =? "Firefox" <&&> resource =? "Download") --> doFloat]
 -- Float Firefox dialog windows
            ,[(className =? "Firefox" <&&> resource =? "Dialog") --> doFloat ]
            ]
@@ -29,10 +39,16 @@ currentWorkspaceColor = "#FF0000"	-- red
 visibleWorkspaceColor = "#1E90FF"	-- dodgerblue 1 (dodgerblue)
 myPP = xmobarPP { ppCurrent = xmobarColor currentWorkspaceColor "" -- . wrap "<" ">"
                 , ppVisible = xmobarColor visibleWorkspaceColor "" . wrap "[" "]"
-                , ppSort    = getSortByXineramaRule
+--                , ppSort    = getSortByXineramaRule
                 , ppSep     = " | "
                 , ppTitle   = xmobarColor "green" "" . shorten 85
+                , ppExtras = [ willFloatAllNewPP (\x-> "Float") ]
+                , ppOrder = newOrder
                 }
+newOrder :: [String] -> [String]
+newOrder (ws:la:ti:[])    = [ws,la,ti]
+newOrder (ws:la:ti:ex:[]) = [ws,la,ex,ti]
+newOrder x                = x
 
 -- Key binding to toggle the gap for the bar.
 toggleStrutsKey XConfig {XMonad.modMask = modMask} = (modMask, xK_b)
@@ -44,8 +60,10 @@ dmenuRunCmd = "cmd=$(yeganesh --executables -- -b -f -i -p \"$ \") && exec $cmd"
 --myConfig = defaultConfig { modMask = mod4Mask }
 myConfig = defaultConfig {
              terminal = terminalCmd
---           , layoutHook = avoidStruts $ layoutHook defaultConfig
-           , manageHook = manageDocks <+> myManageHook <+> manageHook defaultConfig
+           , layoutHook = avoidStruts $ layoutHook defaultConfig
+           , manageHook = placeHook (inBounds (underMouse (0.5,0.5))) <+> -- simpleSmart
+                          floatNextHook <+>
+                          manageDocks <+> myManageHook <+> manageHook defaultConfig
            , startupHook = setWMName "LG3D"
            } `additionalKeys`
            [ ((mod4Mask, xK_l), spawn "xscreensaver-command -lock")
@@ -63,5 +81,7 @@ myConfig = defaultConfig {
            , ((controlMask, 0x1008ff13), spawn "amixer -q set Capture,0 1000+")
              -- Ctrl+XF86AudioLowerVolume=microphone lower volume
            , ((controlMask, 0x1008ff11), spawn "amixer -q set Capture,0 1000-")
+             -- See also http://xmonad.org/xmonad-docs/xmonad-contrib/XMonad-Hooks-FloatNext.html
+           , ((mod1Mask, xK_r), toggleFloatAllNew >> runLogHook)
            ]
 
